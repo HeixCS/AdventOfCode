0a87e8de96030024c65c47b0970a43834f1b63f4
HeixCS <mmasoo08@uoguelph.ca>
2025-12-01 16:39:53 -0500

    adjusted program for part 2

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -19,22 +19,29 @@ vector<string> read_file(string file_name){
     return output_arr;
 }
 
-int rotate(string rot_str, int current_dial){
-    int offset, strlength, result;
+int rotate(string rot_str, int current_dial, int* output){
+    int offset, strlength, result, prev_dial;
     strlength = rot_str.size();
+    prev_dial = current_dial;
     // Converting the second half of the string into int
     offset = stoi(rot_str.substr(1, strlength - 1));
 
+    *output =  *output + ((int)offset / 100);
     // Removing full rotations
-    offset = offset % 99;
+    offset = offset % 100;
 
-    cout << "offset is " << offset << " and " << rot_str[0] << " and " << rot_str << "\n";
     if(rot_str[0] == 'L'){
-        if(offset > current_dial) result = 100 - (offset - current_dial);
+        if(offset > current_dial){
+            result = 100 - (offset - current_dial);
+            if(result != 0 && prev_dial != 0) *output = *output + 1;
+        }
         else result = current_dial - offset;
     }
     else{
-        if(current_dial + offset > 99) result = current_dial + offset - 100;
+        if(current_dial + offset > 99){
+            result = current_dial + offset - 100;
+            if(result != 0 && prev_dial != 0) *output = *output + 1;
+        }
         else result = current_dial + offset;
     }
     return result;
@@ -43,9 +50,14 @@ int rotate(string rot_str, int current_dial){
 int main(void){
     vector<string> str_arr = read_file("input.txt");
     int current_dial = 50;
+    int final_score = 0;
     for(auto str_line : str_arr){
         // cout << "current dial is " << current_dial << "\n";
-        current_dial = rotate(str_line, current_dial);
+        current_dial = rotate(str_line, current_dial, &final_score);
+        if(current_dial == 0){
+            final_score++;
+        }
     }
+    cout << "The final score is " << final_score << endl;
     return 0;
 }
