04b17a9f62a8802be6a823cfad7ab61a18e9b740
HeixCS <mmasoo08@uoguelph.ca>
2025-12-07 08:17:02 -0500

    Finished part 2

--- Day5/main.cpp
+++ Day5/main.cpp
@@ -6,6 +6,7 @@
 #include <experimental/optional>
 #include <cmath>
 #include <sstream>
+#include <complex>
 class file_reader{
     private:
         std::ifstream file_stream;
@@ -29,12 +30,37 @@ class file_reader{
         return output;
     }
 };
+bool check_overlap(std::pair<long long, long long> range1, std::pair<long long, long long> range2){
+    // std::cout << "a:checking1 " << range1.first << " and " << range1.second << "\n";
+    // std::cout << "a:checking2 " << range2.first << " and " << range2.second << "\n";
+    if(range1.second >= range2.first && range1.second <= range2.second) return true;
+    if(range1.first <= range2.second && range1.first >= range2.first) return true;
+
+
+    if(range2.second >= range1.second && range2.first <= range1.first) return true;
+    if(range1.second >= range2.second && range1.first <= range2.first) return true;
+    return false;
+}
+
+std::pair<long long, long long> combine_pairs(std::pair<long long, long long> &range1, std::pair<long long, long long> &range2){
+    std::pair<long long, long long> output;
+    // std::cout << "b:checking1 " << range1.first << " and " << range1.second << "\n";
+    // std::cout << "b:checking2 " << range2.first << " and " << range2.second << "\n";
+    if(range1.first < range2.first) output.first = range1.first;
+    else output.first = range2.first;
+
+    if(range1.second > range2.second) output.second = range1.second;
+    else output.second = range2.second;
+
+    return output;
+}
 
 int main(void){
     file_reader freader("input.txt");
     std::experimental::optional<std::string> line_holder;
     std::string line, lower_range, higher_range;
     std::vector<std::pair<long long, long long>> ranges;
+    std::vector<long long> list_valid_ids;
     const char del = '-';
     int line_size;
     long long lower_range_digit, higher_range_digit;
@@ -43,7 +69,6 @@ int main(void){
         if(line_holder.value() == ""){
             break;
         }
-        // std::cout << line_holder.value() << "\n";
         std::stringstream range_divided(line_holder.value());
         std::getline(range_divided, lower_range, del);
         std::getline(range_divided, higher_range, del);
@@ -51,14 +76,46 @@ int main(void){
         higher_range_digit = std::stoll(higher_range);
         ranges.push_back(std::make_pair(lower_range_digit, higher_range_digit));
     }
-    while(line_holder = freader.get()){
-        long long id = std::stoll(line_holder.value());
-        for(auto range : ranges){
-            if(id <= range.second && id >= range.first){
-                sum++;
-                break;
+    bool changed = true;
+    while(changed){
+        changed = false;
+        // Going through every pair
+        for(int i = 0; i < ranges.size(); i++){
+            std::pair<long long, long long> range = ranges[i], new_pair;
+            std::vector<int> overlapping_ids;
+            // Checking for overlaps
+            for(int j = 0; j < ranges.size(); j++){
+                if(j == i) continue;
+                if(check_overlap(range, ranges[j])) overlapping_ids.push_back(j);
             }
+            // Checking if there were any overlapping ranges
+            if(overlapping_ids.size() > 0) changed = true;
+            else continue;
+            // Combining elements
+            new_pair = range;
+            for(auto check : overlapping_ids){
+                new_pair = combine_pairs(new_pair, ranges[check]);
+            }
+            // Deleting elements
+            bool i_deleted = false;
+            for(int j = overlapping_ids.size() - 1; j >= 0 ; j--){
+                if(!i_deleted && overlapping_ids[j] < i){
+                    i_deleted = true;
+                    ranges.erase(ranges.begin() + i);
+                }
+                ranges.erase(ranges.begin() + overlapping_ids[j]);
+            }
+            if(!i_deleted){
+                ranges.erase(ranges.begin() + i);
+            }
+            // Adding back new range
+            ranges.push_back(new_pair);
+            break;
         }
     }
-    std::cout << "The number of fresh ingredients are " << sum;
+    for(auto range : ranges){
+        sum += range.second - range.first + 1;
+    }
+    std::cout << "Final # of fresh ingredients is " << sum;
+
 }
